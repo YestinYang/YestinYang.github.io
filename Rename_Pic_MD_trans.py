02b30574a94aa41fa5d9c222a501e7f5f917737b
yestinyang <yestinyang88@gmail.com>
2018-03-29 00:34:12 +0800

    feat: Add in script to rename pic
    Used while transforming markdown in Marxico to GitHub, rename picture to "Date + Name"

---
+++ Rename_Pic_MD_trans.py
@@ -0,0 +1,24 @@
+import os
+
+dir = "d:/new post"
+blog_date = "2017-12-04"
+
+def rename_file(dir, blog_date):
+    # 1) get the list of file name
+    file_list = os.listdir(dir)  # r for do not modify the result
+    file_list_png = [i for i in file_list if ".md" not in i]  # get only picture files
+
+    # 1.5) handle working directory
+    saved_dir = os.getcwd()
+    os.chdir(dir)
+    print ("Working directory is changed to " + dir)
+
+    # 2) rename them
+    for file_name in file_list_png:
+        os.rename(file_name, blog_date+"_"+file_name)
+
+    # 2.5) back to original dir
+    print ("Working directory is changed back to " + saved_dir)
+    os.chdir(saved_dir)
+
+rename_file(dir, blog_date)
